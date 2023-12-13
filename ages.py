c08aeed7c1726af9e9b1fd6f40d920001efa23f4
Ahat <ahat.saraiah@gmail.com>
2023-12-13 16:27:58 +0200

    add TestAgesFunction -unit tests

--- ages.py
+++ ages.py
@@ -1,10 +1,17 @@
-name = input("Enter your name:")
-print("Enter your age:")
-age = int(input())
 
-if age < 12:
-    print(name + " is Child")
-elif age >= 12 and age <= 18:
-    print(name +" is Teenager")
-else:
-    print(name +" is Adult")
+
+
+
+def ages():
+    name = input("Enter your name:")
+    print("Enter your age:")
+    age = int(input())
+
+    if age < 12:
+        print(name + " is Child")
+    elif age >= 12 and age <= 18:
+        print(name +" is Teenager")
+    else:
+        print(name +" is Adult")
+
+ages()
